6b3f67b0f156e979221d7ec70f43e91db9ed6bef
zineb zarda <zinebzarda1@gmail.com>
2024-09-27 10:55:57 +0100

    des changements

--- backEnd/task-service/src/main/java/com/ServiceTask/client/ProjectServiceClient.java
+++ backEnd/task-service/src/main/java/com/ServiceTask/client/ProjectServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ServiceGestionProjets")
+@FeignClient(name = "project-service")
 public interface ProjectServiceClient {
 
     @GetMapping("/api/projects/{projectId}/exist")
